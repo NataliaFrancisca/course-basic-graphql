3896761106b34a31edba4a3de46a321e6626db28
NataliaFrancisca <nathifrancisca@gmail.com>
2022-04-06 09:32:15 -0300

    aula 1 e 2

---
+++ graphql-course-basic/src/index.js
@@ -0,0 +1,62 @@
+const { ApolloServer, gql} = require('apollo-server');
+
+const typeDefs = gql`
+
+    type User{
+        _id: ID!
+        name: String!
+        email: String!
+        active: Boolean!
+    }
+
+    type Post{
+        _id: ID!
+        title: String!
+        content: String!
+        author: User!
+    }
+
+    type Query{
+        hello: String
+        users: [User]!
+        getUserByEmail(email: String!): User!
+    }
+
+    type Mutation{
+        createUser(name: String!, email: String!): User!
+    }
+`;
+
+const users = [
+    { _id: String(Math.random()), name: "Nat", email: "nath@mail.com", active: true},
+    { _id: String(Math.random()), name: "Maria", email: "maria@mail.com", active: false},
+    { _id: String(Math.random()), name: "Luan", email: "luan@mail.com", active: true},
+]
+
+const resolvers = {
+    Query: {
+        hello: () => "Hello Word",
+        users: () => users,
+        getUserByEmail: (_,  args) => {
+            return users.find((user) => user.email === args.email)
+        },
+    },
+
+    Mutation: {
+        createUser: (_, args) => {
+            const newUser = {
+                _id: String(Math.random()),
+                name: args.name,
+                email: args.email,
+                active: true
+            };
+
+            users.push(newUser);
+            return newUser;
+        }
+    }
+};
+
+
+const server = new ApolloServer({ typeDefs, resolvers});
+server.listen().then(({ url}) => console.log(`🔥 Server started at ${url}`));
